7f2aa6d90d9b62120c7e23e144b5932b4fe60621
EstherKim97 <181283957+EstherKim97@users.noreply.github.com>
2024-10-03 14:44:44 -0700

    testing

--- largest_prime.py
+++ largest_prime.py
@@ -12,3 +12,40 @@ Task: Find the largest prime Fibonacci number less that 50000
 """
 
 # You're on your own for this one. Good luck!
+
+import sys
+import fibonnaci
+
+def getprimenumber(number):
+	fibonnaci_list = fibonnaci.fibonnaci_function(number)
+
+	prime_no = 0
+
+	for nums in fibonnaci_list:
+		count = 0
+
+		for i in range(1, nums+1):
+			if nums % i == 1:
+				count += 1
+			
+		if count == 2:
+			prime_no = nums
+	
+	return prime_no
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("Usage: python largest_prime.py <upper_limits>")
+		sys.exit(1)
+	
+	upper_limits = int(sys.argv[1])
+
+	largestprimefromfib = getprimenumber(upper_limits)
+
+	print(f"THe largest prime Fibonnaci number less than {upper_limits} is: {largestprimefromfib}.")
+	
+
+
+
+
+
